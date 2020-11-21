a3dd7b63cefe88202092f5a2d19e706148324f42
thorgriffs <22.kelliking@gmail.com>
2020-11-21 14:33:18 -0500

    added save and pull from local storage.  had to refactor the functions to allow the saved cities to also pull from the APIs

--- script.js
+++ script.js
@@ -1,30 +1,90 @@
 // page load that checks and then pulls from localStorage to:
 
+const localStorageKey = "cities";
+var searchedCities = [];
+const maxSearchedCities = 5;
 
+retrieveStoredCities();
+
+console.log("Retrieved " + searchedCities.length + " cities from local storage");
+console.log(searchedCities);
+
+if (searchedCities.length > 0) {
+    showCity(searchedCities[searchedCities.length - 1]);
+}
+
+function retrieveStoredCities() {
+    var cities = localStorage.getItem(localStorageKey);
+    if(!cities) 
+        searchedCities = [];
+    else 
+        searchedCities = JSON.parse(cities);    
+    for (var i = 0; i < searchedCities.length; i++) {
+        var city = searchedCities[i];
+        addSearchedCityBtn(city);
+    }
+}
+
+function storeCities() {
+    localStorage.setItem(localStorageKey, JSON.stringify(searchedCities));
+}
+
+function addCityToHistory(city) {
+    var searchHistory = $("#searchHistory");
+    while (searchedCities.length >= maxSearchedCities) {
+        searchedCities.shift();
+        searchHistory.find("button.history").last().remove();
+    }
+    searchedCities.push(city);
+    storeCities();
+    addSearchedCityBtn(city);
+}
+
+function addSearchedCityBtn(city) {
+    var searchHistory = $("#searchHistory");
+    var lastSearchedCityBtn = $("<button>").attr({"type": "button", "class": "history btn btn-outline-secondary w-100 mr-1"});
+    lastSearchedCityBtn.text(city.name);
+    lastSearchedCityBtn.click(function() {
+        showCity(city);        
+    });
+
+    searchHistory.prepend($("<br>")).prepend(lastSearchedCityBtn);
+}
 
 
 $("#searchBtn").click(function() {
-        var city = $("#citySearch").val().trim();
-        console.log(city);
+        var city = {};
 
-        var cityURL = buildCityURL(city);
+        var cityText = $("#citySearch").val().trim();
+        console.log(cityText);
+
+        var cityURL = buildCityURL(cityText);
 
         $.ajax({
             url: cityURL,
             method: "GET"
         }).then(function(response) {
             console.log(response);
-            $("#cityHeader").text(response.name);
-            $("#temperature").text("Temperature: " + response.main.feels_like);
-            $("#humidity").text("Humidity: " + response.main.humidity);
-            $("#windSpeed").text("Wind Speed: " + response.wind.speed);
-            var uvIndex = buildIndexURL(response.coord.lat, response.coord.lon);
+
+            city.name = response.name;
+            city.feels_like = response.main.feels_like;
+            city.humidity = response.main.humidity;
+            city.windspeed = response.wind.speed;
+            city.lat = response.coord.lat;
+            city.lon = response.coord.lon;
+
+            showCurrentConditions(city);
+
+            var uvIndex = buildIndexURL(city.lat, city.lon);
             $.ajax({
                 url: uvIndex,
                 method: "GET"
             }).then(function(response){
+
                 console.log(response);
-                $("#uvIndex").text("UV Index: " + response.value);
+
+                city.uvIndex = response.value;
+                showUvIndex(city);
             })
             var forecast = buildForecastURL(response.coord.lat, response.coord.lon);
             $.ajax({
@@ -32,26 +92,22 @@ $("#searchBtn").click(function() {
                 method: "GET"
             }).then(function(response) {
                 console.log(response);
+
+                city.days = [];
+
                 for (var i =0; i < 5; i++) {
+
+                    var cityDay = { };
+                    city.days.push(cityDay);
+
                     var day = response.daily[i];
-                    var date = new Date(day.dt * 1000);
-                    var dateDisplay = (date.getMonth() +1) + "/" + date.getDate() + "/" + date.getFullYear();
-
-                    var idNum = i + 1;
-
-                    var dayDate = $("#date" + idNum);
-                    dayDate.text(dateDisplay);
-
-                    var dayTemp = $("#temperature" + idNum);
-                    dayTemp.text((day.temp.day) + "°");
-
-                    var dayConditions = $("#conditions" + idNum);
-                    var weatherIcon = day.weather[0].icon;
-                    dayConditions.attr("src", "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
-
-                    var dayHumid = $("#humidity" + idNum);
-                    dayHumid.text(day.humidity + '%');
+                    cityDay.dt = day.dt;
+                    cityDay.temp = day.temp.day;
+                    cityDay.weatherIcon = day.weather[0].icon;
+                    cityDay.humidity = day.humidity;                   
                 }
+                showDays(city);
+                addCityToHistory(city);
             });                   
         });
         // response.main.name
@@ -60,9 +116,6 @@ $("#searchBtn").click(function() {
         // response.main.humidity
         // response.coord.lat
         // response.coord.lon
-
-   
-
 });
 
 var apiKey = "849dd85b1de6d21e0d35bf2a08ccebea";
@@ -86,6 +139,47 @@ function buildForecastURL(lat, lon) {
     var forecastURL = "https://api.openweathermap.org/data/2.5/onecall?"
 
     return forecastURL + "lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
+}
+
+function showCity(city) {
+    showCurrentConditions(city);
+    showUvIndex(city);
+    showDays(city);
+}
+
+function showCurrentConditions(city) {
+    $("#cityHeader").text(city.name);
+    $("#temperature").text("Temperature: " + city.feels_like);
+    $("#humidity").text("Humidity: " + city.humidity);
+    $("#windSpeed").text("Wind Speed: " + city.windspeed); 
+}
+
+function showUvIndex(city) {
+    $("#uvIndex").text("UV Index: " + city.uvIndex);
+}
+
+function showDays(city) {
+    for (var i = 0; i < 5; i++) {
+        
+        var day = city.days[i];
+
+        var date = new Date(day.dt * 1000);
+        var dateDisplay = (date.getMonth() +1) + "/" + date.getDate() + "/" + date.getFullYear();
+
+        var idNum = i + 1;
+
+        var dayDate = $("#date" + idNum);
+        dayDate.text(dateDisplay);
+
+        var dayTemp = $("#temperature" + idNum);
+        dayTemp.text((day.temp.day) + "°");
+
+        var dayConditions = $("#conditions" + idNum);
+        dayConditions.attr("src", "http://openweathermap.org/img/wn/" + day.weatherIcon + "@2x.png");
+
+        var dayHumid = $("#humidity" + idNum);
+        dayHumid.text(day.humidity + '%');
+    }
 }
 // $("#searchBtn").on("click", function() {
 //     // This line allows us to take advantage of the HTML "submit" property
